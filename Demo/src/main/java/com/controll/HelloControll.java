5ddaa0d464a980a6df03a55d3b845022ecb31d0d
lishicheng@eastcompeace.com <12345678>
2020-05-11 16:13:05 +0800

    test

--- Demo/src/main/java/com/controll/HelloControll.java
+++ Demo/src/main/java/com/controll/HelloControll.java
@@ -1,16 +1,20 @@
 package com.controll;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("hello")
 public class HelloControll {
 
+    @Value("${neo.hello}")
+    private String hello;
+
     @GetMapping("test")
     @ResponseBody
     public String test(@RequestParam String name) {
         System.out.println("ddddd");
-        return "test";
+        return this.hello;
     }
 
 }
